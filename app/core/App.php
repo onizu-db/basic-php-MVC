a35f52d42dcfa4135e08f3a30de61021f7fa9346
dushyant (ms) <onizu.db@gmail.com>
2020-04-06 20:16:51 +0530

    wip

--- app/core/App.php
+++ app/core/App.php
@@ -14,7 +14,11 @@ class App {
         }
         require_once '../app/controllers/'.$this->controller.'.php';
         $this->controller = new $this->controller;
-        var_dump($this->controller);
+        if (isset($url[1])) {
+            if (method_exists($this->controller, $url[1])) {
+                echo 'method exists';
+            }
+        }
     }
 
     public function parseUrl() {
