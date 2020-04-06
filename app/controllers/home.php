e42c65d30a53af9667249f06c733a94b52ee154e
dushyant (ms) <onizu.db@gmail.com>
2020-04-06 22:05:18 +0530

    wip

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,7 +1,9 @@
 <?php
 
 class Home extends Controller {
-    public function index($name = '', $otherName = '') {
-        echo $name . ' ' . $otherName;
+    public function index($name = '') {
+        $user = $this->model('User');
+        $user->name = $name;
+        echo $user->name;
     }
 }
