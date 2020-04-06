3a993cbbb8187eccee0221ebb0afaba894298b3a
dushyant (ms) <onizu.db@gmail.com>
2020-04-06 23:00:53 +0530

    some comments added

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -3,7 +3,7 @@
 class Home extends Controller {
     public function index($name = '') {
         $user = $this->model('User');
-        $user->name = $name;
+        $user->name = $name;  // $name is a public property in $model class
         
         $this->view('home/index', ['name' => $user->name]);
     }
--- app/core/Controller.php
+++ app/core/Controller.php
@@ -3,7 +3,7 @@
 class Controller {
     public function model($model) {
         require_once '../app/models/'. $model .'.php';
-        return new $model();
+        return new $model(); // $model is actually a class in $model.php, so creating a new object of that class
     }
     public function view($view, $data = []) {
         require_once '../app/views/'. $view .'.php';
