2303babeffcfb71d242f11618b5a9781f5df179e
dushyant (ms) <onizu.db@gmail.com>
2020-04-06 20:19:37 +0530

    wip

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -4,4 +4,7 @@ class Home extends Controller {
     public function index() {
         echo 'home/index from home.php in controllers';
     }
+    public function test() {
+        echo 'home/test from home.php in controllers';
+    }
 }
