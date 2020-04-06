1d24a5c978e3f363ce89d25e67562481c9fee6f9
dushyant (ms) <onizu.db@gmail.com>
2020-04-06 20:07:45 +0530

    wip

--- app/core/App.php
+++ app/core/App.php
@@ -5,8 +5,13 @@ class App {
     protected $method = 'index';
     protected $params = [];
     public function __construct() {
-
-        print_r($this->parseUrl());
+        $url = $this->parseUrl();
+        if(file_exists('../app/controllers/'.$url[0].'.php')) {
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+        require_once '../app/controllers/'.$this->controller.'.php';
+        echo $this->controller;
     }
 
     public function parseUrl() {
